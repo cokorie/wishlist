b400768bf85887e19d5de63c9823d9983833ebf7
cokorie <chijioke.okorie@gmail.com>
2021-11-12 15:12:11 -0600

    Add Item

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ItemList.as_view(), name='item_list'),
+    path('items/create/', views.ItemCreate.as_view(), name='item_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-
-from main_app.models import Item
+from django.views.generic.edit import CreateView
+from .models import Item
+from main_app import models
 # Create your views here.
 
 class ItemList(ListView):
     model = Item
+
+class ItemCreate(CreateView):
+    model = Item
+    fields = '__all__'
+    success_url = '/'
 
