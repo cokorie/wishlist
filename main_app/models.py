f14bd1e4d08c3ffc325f01f24191a1f72116b412
cokorie <chijioke.okorie@gmail.com>
2021-11-12 16:13:19 -0600

    Add and Delete Items

--- main_app/models.py
+++ main_app/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 
 class Item(models.Model):
-    description = models.CharField(max_length=300)
+    description = models.TextField(max_length=300)
--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.ItemList.as_view(), name='item_list'),
     path('items/create/', views.ItemCreate.as_view(), name='item_create'),
+    path('items/<int:pk>/delete', views.ItemDelete.as_view(), name='item_delete'),
 ]
